b7180750c8a8a9991b2198236356074393dfd1c2
Ismael <ismael2021ab@gmail.com>
2023-10-13 17:02:16 -0300

    Feat: Let's Go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("HELLO WORLD!")
+
+	var idade int
+	idade = 16
+
+	var nome string
+	nome = "Ismael"
+
+	nome2 := "Another Ismael"
+
+	fmt.Println(idade, nome, nome2)
+}
